82acf21e54b5572f65d2853731d8973555ab6fc6
=Arun <=arunv@student.tce.edu>
2021-02-17 15:02:48 +0530

    v1

---
+++ index.php
@@ -0,0 +1,386 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+    <title>TechLeague</title>
+	<link rel="icon" href="se2i.png">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style type="text/css">
+        * {
+            margin: 0px;
+            padding: 0px;
+            box-sizing: border-box;
+        }
+        #menu {
+            list-style: none;
+            margin: 0;
+            overflow: hidden;
+            display: flex;
+            flex-wrap: wrap;
+            font-size: 80%;
+            padding-right: 607px;
+            padding-top: 05px;
+            padding-bottom: 05px;
+        }
+        #menu2{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            right: 0px;
+            background: rgba(117, 118, 134, 0.51);
+        }
+        #menu li{
+            float:left ;
+            border-width:1px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+        #menu a{
+            color: whitesmoke;
+            font-size: 150%;
+            display :block ;
+            text-decoration :none;
+            align-content:center;
+            padding: 10px 30px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+        #menubg{
+            background-color:crimson
+            display: flex;
+            flex-wrap: wrap;
+        }
+        #menu a:hover {
+            background-color:lightsalmon
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .he1{
+            font-family: Montserrat;
+            text-align: center;
+            font-size: 200%;
+            display: flex;
+            flex-wrap: wrap;
+            padding-top: 200px;
+            padding-left:30%;
+        }
+        .he2{
+            font-family: Montserrat;
+            text-align: center;
+            font-size: 140%;
+            display: flex;
+            flex-wrap: wrap;
+            padding-top: 1px;
+            padding-left:30%;
+        }
+        #headtittle{
+            position: relative;
+            text-align: center;
+            color: whitesmoke;
+            background: rgba(68, 171, 249, 0.84);
+            display: flex;
+            flex-wrap: wrap;
+        }
+        #headtittle img{
+            width: 100%;
+            height: 350px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .HT1{
+            position:absolute ;
+            top: 0px;
+            bottom: 0px;
+            width: 100%;
+            background: rgba(11, 11, 11, 0.75);
+            font-size: 140%;
+            display: flex;
+            flex-wrap: wrap;
+        text-align: center;
+        }
+        .sei2{
+            position: absolute;
+            padding-left: 10%;
+        }
+        .se2t{
+            position:relative ;
+            padding-left: 32%;
+             font-family: Montserrat;
+             font-size: 130%;
+            font-weight: bold;
+            padding-top: 0px;
+            border-style: none;
+        }
+        .se2{
+            padding-top: 45px;
+            padding-bottom: 45px;
+            font-family: Montserrat
+            font-size: 110%;
+            background: rgba(238, 209, 200, 0.88);
+        }
+        body{
+            background:white;
+        }
+        .upd{
+            padding-top: 8px;
+            padding-bottom: 8px;
+            background-color: #8c99e0;
+            color: white;
+            text-align: center;
+            font-family: "Cambria Math";
+            font-size: 150%;
+        }
+        .bt1{
+            font-size: 130%;
+            font-weight: bold;
+            font-family: Montserrat;
+            border-width: 0px;
+            margin: 0px;
+            padding: 0px;
+            border-style: none;
+            background-color: #3dc646;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: white;
+        }
+        #btf1{
+            border-right-style: solid;
+            border-right-width: 60px;
+            border-bottom-style: solid;
+            border-bottom-width: 30px;
+            border-color:whitesmoke;
+            background-color: rgba(201, 194, 138, 0.35);
+            padding-top: 0px;
+            padding-left: 0px;
+            padding-right: 0px;
+            padding-bottom: 25px;
+            text-align: center;
+            font-size: 115%;
+            color: white;
+        }
+        #btf1 a{
+            text-decoration :none;
+            color: #070267;
+        }
+        #btf1 a:hover{
+            color: #636567;
+        }
+        .vn1 img{
+            height: 140px;
+            width: 260px;
+        }
+        .vtt1{
+            padding-top: 5px;
+            font-size: 120%;
+            padding-bottom: 10px;
+        }
+        .vtp1{
+                   padding-top: 5px;
+                   padding-bottom: 0px;
+                   padding-left: 1px;
+                   padding-right: 1px;
+               }
+
+        @media(max-width: 780px) {
+            #headtittle img{
+                height:230px;
+            }
+            .HT1{
+                font-size: 95%;
+            }
+            .he1{
+                font-size: 170%;
+                padding-left: 25%;
+                padding-top: 70px;
+            }
+            .he2 {
+                padding-top: 1px;
+                padding-left: 23%;
+                font-size: 130%;
+            }
+            .HT1{
+                padding-left: 0%;
+            }
+            .se2t{
+                font-size: 95%;
+            }
+            .sei2{
+                padding-top: 0px;
+                padding-left: 05%;
+
+            }
+            .se2{
+                padding-top: 30px;
+                padding-bottom: 30px;
+            }
+            img{
+                height:165px;
+                width: 165px;
+            }
+            #menu{
+                font-size: 90%;
+                padding-right: 0px;
+            }
+            #menu a {
+                padding-top: 0px;
+                padding-left: 10px;
+                padding-right: 10px;
+                padding-bottom: 0px;
+            }
+            }
+            @media (max-width: 480px) {
+                #headtittle img{
+                    height:180px;
+                }
+                .HT1{
+                    font-size: 75%;
+                }
+                .he1{
+                    font-size: 150%;
+                    padding-left: 20%;
+                    padding-top: 120px;
+                }
+                .he2 {
+                    padding-top: 1px;
+                    padding-left: 17%;
+                    font-size: 120%;
+                }
+                .HT1{
+                    padding-left: 0%;
+                }
+                .se2t{
+                font-size: 78%;
+                }
+                .sei2{
+                    padding-top: 15px;
+                  padding-left: 0%;
+                }
+                .se2{
+                    padding-top: 30px;
+                    padding-bottom: 30px;
+                }
+                .sei2{
+                    padding-left: 0%;
+                }
+                img{
+                    height:150px;
+                    width: 120px;
+                }
+            #menu{
+                font-size: 68%;
+                padding-right: 0px;
+            }
+            #menu a {
+                padding-top: 0px;
+                padding-left: 5px;
+                padding-right: 5px;
+                padding-bottom: 0px;
+            }
+                #btf1{
+                border-right-style: solid;
+                border-left-style: solid;
+                padding-bottom: 5px;
+                padding-left: 0px;
+                padding-top: 0px;
+                border-left-width: 0px;
+                border-right-width: 10px;
+                width: 50%;
+                float: left;
+                    font-size: 90%;
+                }
+                .bt1{
+                    font-size: 100%;
+                }
+                .upd{
+                    padding-top: 5px;
+                    padding-bottom: 5px;
+                    font-size: 120%;
+                }
+            }
+        @media (max-width: 320px) {
+            #headtittle img{
+                height:170px;
+            }
+            .HT1{
+                font-size: 70%;
+            }
+            .he1{
+                font-size: 120%;
+                padding-left: 18%;
+                padding-top: 65px;
+            }
+            .he2 {
+                padding-top: 1px;
+                padding-left: 16%;
+                font-size: 97%;
+            }
+            .HT1{
+                padding-left: 0%;
+            }
+            .se2t{
+                font-size: 65%;
+            }
+            .sei2{
+                padding-top: 13px;
+                padding-left: 0%;
+            }
+            .se2{
+                padding-top: 27px;
+                padding-bottom: 27px;
+            }
+            .sei2{
+                padding-left: 0%;
+            }
+            img{
+                height:150px;
+                width: 110px;
+            }
+            #menu{
+                font-size: 50%;
+                padding-right: 0px;
+            }
+            #menu a {
+                padding-top: 0px;
+                padding-left: 5px;
+                padding-right: 5px;
+                padding-bottom: 0px;
+            }
+        }
+    </style>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<div id="headtittle">
+    <span><img src="he1.jpg"></span>
+    <div class="HT1">
+        <div class="he1"><span>WELCOME TO TECHLEAGUE</span> </div>
+        <div class="he2"><span>Learning Is The Steps To Archive Success</span> </div>
+    </div>
+    <div id="menu2">
+        <ol id="menu" >
+            <li><a id="menubg" href=" ">Home</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Softwares</a></li>
+            <li><a href="#">Blogs</a></li>
+<li> <a href="#">Questions</a></li>
+            <li> <a href="#">Contact Us</a></li>
+        </ol>
+    </div>
+</div>
+<div class="se2">
+    <span class="sei2"><img src="se2i.png" height ="160" width ="160"></span>
+<div class="se2t">
+This Site is build to Develope the learners and students to know more about Technology<br><br>
+This Site has Technology Developement Tutorial Videos and Much More<br><br>
+ Tips And Tricks About The Computing Devices <br><br>
+    If You Have Any Doubts Feel Free To Contact <br><br>
+  Technology Related Questions Are To Be Answered As Soon As Possible<br><br>
+</div>
+</div>
+</body>
+</html>
+<?php
+echo "";
+?>		
